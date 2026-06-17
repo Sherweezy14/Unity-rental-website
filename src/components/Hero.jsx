4693380b9e71ce308d11d509a99d6481ec0b3663
Sherwyn <wedydit@gmail.com>
2026-06-17 03:29:58 -0400

    Update styling on nav and hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,38 @@
+import { FaCalendarAlt, FaDollarSign } from "react-icons/fa";
+
 export default function Hero() {
     return (
       <section
-        className="min-h-[700px] bg-cover bg-center flex items-center"
-        style={{
-          backgroundImage:
-            "linear-gradient(to right, rgba(255,250,242,.95), rgba(255,250,242,.6)), url('/hero.jpg')",
-        }}
-      >
-        <div className="max-w-3xl px-8 md:px-20">
-          <h1 className="text-5xl md:text-7xl font-serif text-slate-900 mb-6">
-            Boston's Affordable Venue For Events Up To 500 Guests
-          </h1>
-  
-          <div className="space-y-3 text-lg">
-            <p>✓ Full Bar & Liquor License</p>
-            <p>✓ Commercial Kitchen</p>
-            <p>✓ Free Parking</p>
-            <p>✓ Dorchester, Boston</p>
-          </div>
-  
-          <div className="flex flex-col md:flex-row gap-4 mt-8">
-            <button className="bg-amber-600 text-white px-6 py-3 rounded">
-              Schedule A Tour
-            </button>
-  
-            <button className="border border-amber-600 px-6 py-3 rounded">
-              Get Pricing
-            </button>
-          </div>
-        </div>
-      </section>
+  className="min-h-[550px] bg-cover bg-center flex items-center"
+  style={{
+    backgroundImage:
+      "url('src/assets/Screen Shot 2026-06-17 at 1.38.05 AM.png')",
+  }}
+>
+  <div className="ml-8 md:ml-20 bg-white/90 p-8 rounded-xl max-w-xl shadow-xl">
+
+    <h1 className="text-4xl md:text-5xl font-serif text-slate-900 mb-6">
+      Boston's Affordable  <br />Venue For Events  <br />Up To 500 Guests
+    </h1>
+
+    <div className="space-y-3 text-lg">
+      <p> <span className="text-amber-600"> ✓ </span> Full Bar & Liquor License</p>
+      <p> <span className="text-amber-600"> ✓ </span> Commercial Kitchen</p>
+      <p> <span className="text-amber-600"> ✓ </span> Free Parking</p>
+      <p> <span className="text-amber-600"> ✓ </span> Dorchester, Boston</p>
+    </div>
+
+    <div className="flex flex-col md:flex-row gap-4 mt-8">
+      <button className="flex items-center bg-amber-600 text-white px-6 py-3 rounded">
+      <span className="text-white mr-1"> <FaCalendarAlt /> </span> Schedule A Tour
+      </button>
+
+      <button className="flex items-center border border-b-black px-6 py-3 rounded">
+      <span className="text-amber-600 mr-1"> <FaDollarSign /> </span> Get Pricing
+      </button>
+    </div>
+
+  </div>
+</section>
     );
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
+import { FaPhone } from "react-icons/fa";
 
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         
-        <div>
-          <h1 className="text-3xl font-serif tracking-widest text-slate-900">
+        <div className="text-center">
+          <h1 className="text-5xl font-serif tracking-widest text-slate-900">
             UNITY
           </h1>
-          <p className="text-xs uppercase tracking-[4px] text-amber-600">
-            Event & Banquet Hall
+          <p className="text-xs uppercase tracking-[1px] text-amber-600">
+            - <span className="text-blue-950">Event & Banquet</span>  Hall - 
           </p>
         </div>
 
@@ -19,12 +20,21 @@ export default function Navbar() {
           <Link to="/weddings">Weddings</Link>
           <Link to="/spaces">Venue Spaces</Link>
           <Link to="/pricing">Pricing</Link>
+          <Link to="/contact">Reviews</Link>
+          <Link to="/contact">About</Link>
           <Link to="/contact">Contact</Link>
         </nav>
-
-        <button className="bg-amber-600 hover:bg-amber-700 text-white px-5 py-3 rounded-lg transition">
+        <div className="text-center">
+ 
+          <a className="flex  items-center gap-1" href="tel:6172698383" >
+          <span className="text-amber-600"> <FaPhone /> </span>
+            (617) 269-8383
+          </a>
+          
+          <button className="bg-amber-600 hover:bg-amber-700 text-white px-5 py-3 rounded-lg transition">
           Schedule Tour
-        </button>
+          </button>
+        </div>
       </div>
     </header>
   );
